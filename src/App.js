0688fd2d5939a8f2745ae4319c526adf13549f19
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-03-13 19:28:05 +0500

    Face identification Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-// import Particles from 'react-particles-js';
-import Particles from "react-tsparticles";
+import React, { Component } from 'react';
+import Particles from 'react-tsparticles';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '01a3395be0d94471a174411513d505ed'
+ });
 
 const particlesOptions = {
   fpsLimit: 120,
@@ -55,7 +60,7 @@ const particlesOptions = {
       direction: "none",
       enable: true,
       outMode: "bounce",
-      random: false,
+      random: true,
       speed: 2,
       straight: false,
     },
@@ -80,35 +85,81 @@ const particlesOptions = {
   detectRetina: true,
 }
 
-const App = () => {
+class App extends Component {
 
-  const particlesInit = (main) => {
-    //console.log(main);
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
 
-  const particlesLoaded = (container) => {
-    //console.log(container);
-  };
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
 
-  return (
-    <div className="App">
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
 
-        <Particles
-          id="tsparticles" 
-          className="particles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={particlesOptions}
-        />  
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-       {/*
-       <FaceRecognition />*/}
-    </div>
-  );
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  onButtonSubmit = () =>{
+    this.setState({ imageUrl: this.state.input });
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then( response => this.displayFaceBox(this.calculateFaceLocation(response)) )
+      .catch( err => console.log(err) );
+      //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+  }
+
+  render(){
+
+    const particlesInit = (main) => {
+      //console.log(main);
+      // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    };
+    
+    const particlesLoaded = (container) => {
+      //console.log(container);
+    };
+
+    return (
+      <div className="App">
+  
+          <Particles
+            id="tsparticles" 
+            className="particles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={particlesOptions}
+          />  
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit} 
+          />
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
