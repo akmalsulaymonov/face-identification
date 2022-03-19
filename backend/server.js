f0e446744aa3e5f741af079b076bd2a318a01cd2
Akmal Sulaymonov <akmalsulaymonov@gmail.com>
2022-03-19 13:40:57 +0500

    signin and register

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,59 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Azizjon',
+            email: 'azizjon@gravity.studio',
+            password: '123456',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Mehrona',
+            email: 'mehrona@gravity.studio',
+            password: '123456',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) => {
-    res.send('working....');
+    res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json('success');
+    }
+    else{
+        res.status(400).json('error loggin in');
+    }
+});
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    });
+
+    res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3001, () =>{
-    console.log('app is ready');
+    console.log('app is running on port 3001');
 });
 
 
